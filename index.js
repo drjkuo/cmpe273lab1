2a50882d3f1e76e84589549341258698f983b429
KUO <drjkuo@gmail.com>
2017-02-14 00:24:51 -0800

    modules

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser');
 var app = express();
 var udatas = [];
 var previousid = 0;
+var cors = require('cors');
+
+
+app.use(cors());
 
 // create application/json parser
 // var jsonParser = bodyParser.json();
@@ -10,11 +14,13 @@ var previousid = 0;
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// POST /login gets urlencoded bodies
+//if the connecting client changes, clear then add data; otherwiese, simply add data
 app.post('/', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400);
 
-  res.send('welcome, ' + req.body.uid);
+  var msg = 'Client No' + req.body.uid + ' entered: ' + req.body.udata;
+  res.send(msg);
+  console.log(msg);
   if (previousid !== req.body.uid) {
     udatas = [];
     previousid = req.body.uid;
@@ -22,11 +28,13 @@ app.post('/', urlencodedParser, function (req, res) {
   udatas.push(req.body.udata);
 });
 
-// POST /api/users gets JSON bodies
-app.post('/kill', urlencodedParser, function (req, res) {
+//kill and print all data stored in the server
+app.get('/kill', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
-    res.send(udatas);
-    udatas = [];
+  console.log('Clear Data from Client No' + previousid);
+  console.log('Data killed: ' + udatas);
+  res.send(udatas);
+  udatas = [];
 
 });
 
